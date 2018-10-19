a45280e12803ac6e63070dd941d1e3611e95496f
EugeneKostyuk <e.kostyuk@zubi-solutions.de>
2018-10-19 16:31:00 +0300

    Added output text on console

--- testGit/Main.cpp
+++ testGit/Main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main() 
 {
+	cout << "New commit" << endl;
 	cout << MyMath::Operations::sum(10, 2) << endl;
 	cout << MyMath::Operations::sub(10, 2) << endl;
 	cout << MyMath::Operations::mult(10, 2) << endl;
