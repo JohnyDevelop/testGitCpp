84768f825c93bcdb39b0fafc7d88d2b2a847584f
JohnyDevelop <41087199+JohnyDevelop@users.noreply.github.com>
2018-10-19 22:02:11 +0300

    Added Hello world display text

--- testGit/Main.cpp
+++ testGit/Main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main() 
 {
+	cout << "Hello world" << endl;
 	cout << "New commit" << endl;
 	cout << MyMath::Operations::sum(10, 2) << endl;
 	cout << MyMath::Operations::sub(10, 2) << endl;
